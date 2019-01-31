0bde49762edc4497bc0b5d36770bfdcfc85120c2
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2019-01-31 16:23:18 +0700

    extract base layout template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,7 +14,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl", "./ui/html/base.layout.tmpl")
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
