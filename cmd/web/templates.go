0f200348bb7893fdfa8321b22a974b4e82157b0e
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2019-02-10 15:45:03 +0700

    refactor form validation

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"github.com/skyfoxs/snippetbox/pkg/forms"
 	"github.com/skyfoxs/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
