223d977bb0391a4cb6847b1492a96a1b50984d70
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2026-07-19 14:37:54 +0700

    Change minimum log level

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,9 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
 
 	mux := http.NewServeMux()
 
